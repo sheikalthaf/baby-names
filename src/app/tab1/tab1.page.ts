3be6291e35f22c19c468de6ac0ae365404fe88f9
Sheik Althaf <althafmech@outlook.com>
2019-03-16 14:15:13 +0530

    initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  itemsClone: { id: number; value: string }[];
+  items = Array(500)
+    .fill('Ionic Documentation')
+    .map((v, i) => ({ id: i + 1, value: v + ' ' + (i + 1) }));
+  users: any[] = [];
+
+  constructor(private http: HttpClient) {
+    this.itemsClone = [...this.items];
+  }
+
+  trackByIdentity(index: number, item: { id: number; value: string }) {
+    return item.id;
+  }
+
+  test(ev: CustomEvent) {
+    const value = ev.detail.value;
+    this.items = this.itemsClone.filter(e =>
+      value ? e.value.toLowerCase().indexOf(value) !== -1 : true
+    );
+  }
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page implements OnInit {
+  users: any[] = [];
+  page = 1;
+  maxPage = 4;
+
+  constructor(private http: HttpClient) {
+    this.getUser();
+  }
+
+  ngOnInit() {
+    console.log('test Message');
+  }
+
+  trackByIdentity(index: number, item: { id: { value: string } }) {
+    return item.id.value;
+  }
+
+  getUser(infiniteScroll?: IonInfiniteScroll) {
+    this.http
+      .get<any>(`https://randomuser.me/api/?results=20&page=${this.page}`)
+      .subscribe(user => {
+        this.users = [...this.users, ...user.results];
+        if (infiniteScroll) {
+          infiniteScroll.complete();
+        }
+      });
+  }
+
+  loadMore(infiniteScroll: CustomEvent<IonInfiniteScroll>) {
+    if (this.page > this.maxPage - 1) return;
+
+    this.page++;
+    this.getUser((infiniteScroll.target as unknown) as IonInfiniteScroll);
+  }
+}
